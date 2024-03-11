ffb83ae795ca7e1c9c1147b0d8a4562ab96ec758
fabiula <150807884+terezafabiula@users.noreply.github.com>
2024-03-11 11:01:41 -0300

    Add files via upload

---
+++ components/Title.js
@@ -0,0 +1,23 @@
+import {View, Text, StyleSheet} from 'react-native'
+
+export function Title( ){
+  return(
+    <View>
+
+      <Text style = {styles.titulo}> UNIVERSIDADE FEDERAL DO CEARÁ</Text>
+      <Text>  É uma instituição de ensino superior pública brasileira, localizada no Estado do Ceará. A UFC é considerada a melhor universidade das regões Norte e Nordeste.</Text>
+
+    </View>
+  )
+}
+
+const styles = StyleSheet.create ({
+  titulo:{
+    backgroundColor: '#22B2ED',
+    fontSize: 25,
+    textAlign: 'center',
+    fontWeight: 'bold',
+    color: '#ffffff',
+    padding: 20,
+  }
+})
